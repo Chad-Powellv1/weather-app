6286b00e1a14887d0326e9bbf0a3e0b6f31a9635
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-03 23:23:07 +0000

    update working main.js

--- js/main.js
+++ js/main.js
@@ -1,84 +1,77 @@
 let getWeatherBtn = document.getElementById('getWeather');
+let zipcode;
+let view = 0;
 
-// create variables for zipcode, modalError, and view
-let state = {
-	zipcode: [],
-	modalError: `The weather information is currently unavailable, please try again later.`,
-	view: 0,
-    
-};  
+// create function to valid zip
+const zipValidation = function () {
+    zipcode = document.getElementById('zipInput').value;
 
-
-
-// connect to DOM to retrieve the input value for the zipcode from end-user
-document.getElementById('zipInput').onchange = function (e) {
-	zipcode = e.target.value;
-	if (zipcode.length === 5 && isNaN(zipcode) === false) {
-		state.zipcode.push(zipcode);
+    if (zipcode.length === 5 && isNaN(zipcode) === false) {
 		console.log('good zip');
-        getWeather();
-    
+        return true;
+           
 	} else {
-		// display zip-error
 		console.log('bad zip');
 	}
 };
+
 
 // create the axios call function to API
 async function getWeather() {
 
 	try {
-		let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${state.zipcode},us&appid=5e8e7816fa1d045c67283a38b2a5b5a6`);
-        updateView(resp)
+		let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=5e8e7816fa1d045c67283a38b2a5b5a6`);
+        let data = resp.data;
 		console.log(resp);
 	} catch (err) {
-		// display modal error message
 		console.log(err);
 	}
 }
 
-
+// create event listener to run zip validation and get weather conditionally
+getWeatherBtn.addEventListener('click', () => {
+    if(zipValidation() === true) {
+        getWeather();
+    }
+});
 
 // create the function to control the state
-const updateState = function () {
-    view++;
-}
 
 // create the function to control the view
-const updateView = function (resp) {
+// const updateView = function (resp) {
 
-    let temperature = resp.main.temp;
-    let fahrenheit = Math.round((celsius * 1.8) + 32)
-    let celsius = Math.round(temperature - 273);
-    let city = resp.name;
-    let weather = resp.weather[0].description;
-    let weatherIcon = resp.weather[0].icon;
+//     let temperature = resp.main.temp;
+//     let fahrenheit = Math.round((celsius * 1.8) + 32)
+//     let celsius = Math.round(temperature - 273);
+//     let city = resp.name;
+//     let weather = resp.weather[0].description;
+//     let weatherIcon = resp.weather[0].icon;
 
-    switch (view) {
+//     switch (view) {
 
-        case 0:
-            appTitle.style.visibility = 'visible';
-            zipInput.style.visibility = 'visible';
-            getWeatherBtn.style.visibility = 'visible';
-            break;
-        case 1:
-            cityDisplay.style.visibility = 'visible';
-            cityDisplay.textContent = city;
-            temp.style.visibility = 'visible';
-            kelvin.textContent = temperature;
-            fahrenheit.textContent = fahrenheit;
-            celsius.textContent = celsius;
-            currentCondition.style.visibility = 'visible';
-            conditions.textContent = weather;
-            image.innerHTML = weatherIcon;
-            break;
+//         case 0:
+//             appTitle.style.visibility = 'visible';
+//             zipInput.style.visibility = 'visible';
+//             getWeatherBtn.style.visibility = 'visible';
+//             break;
+//         case 1:
+//             cityDisplay.style.visibility = 'visible';
+//             cityDisplay.textContent = city;
+//             temp.style.visibility = 'visible';
+//             kelvin.textContent = temperature;
+//             fahrenheit.textContent = fahrenheit;
+//             celsius.textContent = celsius;
+//             currentCondition.style.visibility = 'visible';
+//             conditions.textContent = weather;
+//             image.innerHTML = weatherIcon;
+//             break;
 
-        case 2:
+//         case 2:
 
-
-        
-
-        }
 
         
-}
+
+//         }
+
+        
+// }
