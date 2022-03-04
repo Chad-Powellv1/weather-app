34a320af2736d161bc74e386519f28de638a17cb
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-04 02:15:54 +0000

    update

--- js/main.js
+++ js/main.js
@@ -12,28 +12,26 @@ let conditions = document.getElementById('conditions');
 let imageIcon = document.getElementById('image');
 let zipcode;
 
-
 // create function to valid zip
 const zipValidation = function () {
-    zipcode = document.getElementById('zipInput').value;
+	zipcode = document.getElementById('zipInput').value;
 
-    if (zipcode.length === 5 && isNaN(zipcode) === false) {
+	if (zipcode.length === 5 && isNaN(zipcode) === false) {
 		console.log('good zip');
-        return true;
-           
+		return true;
 	} else {
 		console.log('bad zip');
 	}
 };
 
-
 // create the axios call function to API
 async function getWeather() {
-
 	try {
-		let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=5e8e7816fa1d045c67283a38b2a5b5a6`);
-        let data = await resp.data;
-        updateState(data)
+		let resp = await axios.get(
+			`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=5e8e7816fa1d045c67283a38b2a5b5a6`
+		);
+		let data = await resp.data;
+		updateState(data);
 		console.log(resp);
 	} catch (err) {
 		console.log(err);
@@ -42,65 +40,45 @@ async function getWeather() {
 
 // create event listener to run zip validation and get weather conditionally
 getWeatherBtn.addEventListener('click', () => {
-    if(zipValidation() === true) {
-        getWeather();
-        
-    }
+	if (zipValidation() === true) {
+		getWeather();
+	}
 });
 
-
 const updateState = function (data) {
+	// data variables
+	let temperature = Math.round(data.main.temp);
+	let celsius = Math.round(temperature - 273);
+	let fahrenheit = Math.round(celsius * 1.8 + 32);
+	let city = data.name;
+	let weather = data.weather[0].description;
 
-    // data variables
-    let temperature = Math.round(data.main.temp);
-    let celsius = Math.round(temperature - 273);
-    let fahrenheit = Math.round((celsius * 1.8) + 32)
-    let city = data.name;
-    let weather = data.weather[0].description;
- 
-   
+	// default view
+	appTitle.display = 'block';
+	zipInput.display = 'block';
+	getWeatherBtn.style.display = 'block';
 
+	// weather display
+	cityDisplay.style.display = 'block';
+	cityDisplay.textContent = city;
+	temp.style.display = 'block';
+	kelvin.textContent = `${temperature} °K`;
+	fahrenheitText.textContent = `${fahrenheit} ℉`;
+	celsiusText.textContent = `${celsius} ℃`;
+	currentCondition.style.display = 'block';
+	conditions.textContent = weather;
 
-    // default view
-    appTitle.display = 'block';
-    zipInput.display = 'block';
-    getWeatherBtn.style.display = 'block';
+	// modal error view
+};
 
-    // weather display
-    cityDisplay.style.display = 'block';
-    cityDisplay.textContent = city;
-    temp.style.display = 'block';
-    kelvin.textContent = `${temperature} °K`;
-    fahrenheitText.textContent = `${fahrenheit} ℉`;
-    celsiusText.textContent = `${celsius} ℃`;
-    currentCondition.style.display = 'block';
-    conditions.textContent = weather;
-
-
-    // modal error view
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById('zipInput').addEventListener('click', () => {
+    zipInput.value = '';
+	cityDisplay.style.display = 'none';
+	cityDisplay.textContent = '';
+	temp.style.display = 'none';
+	kelvin.textContent = '';
+	fahrenheitText.textContent = '';
+	celsiusText.textContent = '';
+	currentCondition.style.display = 'none';
+	conditions.textContent = '';
+});
